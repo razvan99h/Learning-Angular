fa4639b4863f6e1104b179d9b98598c8d7b295e7
razvan.horhat <razvan.horhat@accesa.eu>
2020-07-20 15:32:50 +0300

    reservation - basic Firebase service working

--- cinema-app/src/app/shared/services/reservation.service.ts
+++ cinema-app/src/app/shared/services/reservation.service.ts
@@ -1,35 +1,26 @@
 import { Injectable } from '@angular/core';
 import { Seat } from '../models/seat.model';
-import { Observable } from 'rxjs';
+import { Observable, of } from 'rxjs';
 import { AngularFireDatabase } from '@angular/fire/database';
+import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ReservationService {
-  private dummySeats: Seat[];
 
   constructor(
     public db: AngularFireDatabase
   ) {
-    const seats = [];
-    seats.push(new Seat(1, 1));
-    seats.push(new Seat(1, 0));
-    seats.push(new Seat(0, 5));
-    this.dummySeats = seats;
   }
 
   getBookedSeats(): Observable<Seat[]> {
-    console.log("xxxxxxxx", this.dummySeats);
-
-    return new Observable<Seat[]>(observer => {
-      observer.next(this.dummySeats);
-      return observer;
-    });
+    return this.db.list<Seat>('reservations').valueChanges();
   }
 
   bookSeats(seats: Seat[]): void {
-    this.dummySeats.push(...seats);
-    console.log("ddddddddd", this.dummySeats);
+    seats.forEach(seat => {
+      this.db.list('reservations').push(seat);
+    });
   }
 }
